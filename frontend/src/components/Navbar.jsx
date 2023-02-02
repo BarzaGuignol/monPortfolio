826884832086417c2f04f5b7e02f17f05f2b59aa
BarzaGuignol <yayav.yaya@gmail.com>
2023-02-02 20:31:05 +0100

    mise en place de la barre de navigation

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,7 @@
-import React, { useState, useContext, useEffect } from "react";
-import { Link, useNavigate, useLocation } from "react-router-dom";
-import Logo from "@assets/logo.svg";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Navbar() {
-
   const [isOpen, setIsOpen] = useState(false);
   const location = useLocation();
   useEffect(() => {
@@ -15,39 +13,53 @@ export default function Navbar() {
       <div className="hidden sm:flex justify-between p-8 bg-[#1C2833] font-bold">
         <Link to="/">
           {/* <img className="ml-10 mt-3 aspect-auto" src={Logo} alt="logo" /> */}
-          <h2 className="text-4xl text-[#3498DB]">Yanis Viot <strong className="text-[#FFC300]">.</strong></h2>
+          <h2 className="text-4xl text-[#3498DB]">
+            Yanis Viot <strong className="text-[#FFC300]">.</strong>
+          </h2>
         </Link>
         <div>
           <div className="inline-block text-center place-content-center items-center mr-8">
             <Link to="/">
-              <p className="text-2xl text-[#3498DB] hover:text-[#FFC300] hover:underline">Accueil</p>
+              <p className="text-2xl text-[#3498DB] hover:text-[#FFC300] hover:underline">
+                Accueil
+              </p>
             </Link>
           </div>
           <div className="inline-block text-center place-content-center mr-8">
             <Link to="/curriculumvitae">
-              <p className="text-2xl text-[#3498DB] hover:text-[#FFC300] hover:underline">CV</p>
+              <p className="text-2xl text-[#3498DB] hover:text-[#FFC300] hover:underline">
+                CV
+              </p>
             </Link>
           </div>
           <div className="inline-block text-center place-content-center mr-8">
             <Link to="/projets">
-              <p className="text-2xl text-[#3498DB] hover:text-[#FFC300] hover:underline">Projets</p>
+              <p className="text-2xl text-[#3498DB] hover:text-[#FFC300] hover:underline">
+                Projets
+              </p>
             </Link>
           </div>
           <div className="inline-block text-center place-content-center mr-8">
             <Link to="/passion">
-              <p className="text-2xl text-[#3498DB] hover:text-[#FFC300] hover:underline">Passion</p>
+              <p className="text-2xl text-[#3498DB] hover:text-[#FFC300] hover:underline">
+                Passion
+              </p>
             </Link>
           </div>
           <div className="inline-block text-center place-content-center mr-8">
             <Link to="/contact">
-              <p className="text-2xl text-[#3498DB] hover:text-[#FFC300] hover:underline">Contact</p>
+              <p className="text-2xl text-[#3498DB] hover:text-[#FFC300] hover:underline">
+                Contact
+              </p>
             </Link>
           </div>
         </div>
       </div>
       <div className="sm:hidden relative flex bg-[#1C2833] justify-between">
         <Link to="/">
-          <h2 className="text-4xl text-[#3498DB] mt-2">Yanis Viot <strong className="text-[#FFC300]">.</strong></h2>
+          <h2 className="text-4xl text-[#3498DB] mt-2">
+            Yanis Viot <strong className="text-[#FFC300]">.</strong>
+          </h2>
         </Link>
         <button
           className="p-4 space-y-2"
@@ -55,21 +67,25 @@ export default function Navbar() {
           type="button"
         >
           <span
-            className={`${isOpen ? "hidden" : "block"
-              } block w-8 h-1 bg-[#3498DB] rounded-md`}
+            className={`${
+              isOpen ? "hidden" : "block"
+            } block w-8 h-1 bg-[#3498DB] rounded-md`}
           />
           <span
-            className={`${isOpen ? "hidden" : "block"
-              } block w-8 h-1 bg-[#3498DB] rounded-md`}
+            className={`${
+              isOpen ? "hidden" : "block"
+            } block w-8 h-1 bg-[#3498DB] rounded-md`}
           />
           <span
-            className={`${isOpen ? "hidden" : "block"
-              } block w-8 h-1 bg-[#3498DB] rounded-md`}
+            className={`${
+              isOpen ? "hidden" : "block"
+            } block w-8 h-1 bg-[#3498DB] rounded-md`}
           />
         </button>
         <div
-          className={`${isOpen ? "block" : "hidden"
-            } absolute top-0 left-0 w-full h-full z-40 bg-white`}
+          className={`${
+            isOpen ? "block" : "hidden"
+          } absolute top-0 left-0 w-full h-full z-40 bg-white`}
         >
           <div className="relative w-full bg-[#1C2833]">
             <button
@@ -81,19 +97,29 @@ export default function Navbar() {
             </button>
             <div className="p-10">
               <Link to="/">
-                <p className="text-2xl text-[#3498DB] hover:text-[#FFC300] hover:underline py-2">Accueil</p>
+                <p className="text-2xl text-[#3498DB] hover:text-[#FFC300] hover:underline py-2">
+                  Accueil
+                </p>
               </Link>
               <Link to="/curriculumvitae">
-                <p className="text-2xl text-[#3498DB] hover:text-[#FFC300] hover:underline py-2">CV</p>
+                <p className="text-2xl text-[#3498DB] hover:text-[#FFC300] hover:underline py-2">
+                  CV
+                </p>
               </Link>
               <Link to="/projets">
-                <p className="text-2xl text-[#3498DB] hover:text-[#FFC300] hover:underline py-2">Projets</p>
+                <p className="text-2xl text-[#3498DB] hover:text-[#FFC300] hover:underline py-2">
+                  Projets
+                </p>
               </Link>
               <Link to="/passion">
-                <p className="text-2xl text-[#3498DB] hover:text-[#FFC300] hover:underline py-2">Passion</p>
+                <p className="text-2xl text-[#3498DB] hover:text-[#FFC300] hover:underline py-2">
+                  Passion
+                </p>
               </Link>
               <Link to="/contact">
-                <p className="text-2xl text-[#3498DB] hover:text-[#FFC300] hover:underline py-2">Contact</p>
+                <p className="text-2xl text-[#3498DB] hover:text-[#FFC300] hover:underline py-2">
+                  Contact
+                </p>
               </Link>
             </div>
           </div>
