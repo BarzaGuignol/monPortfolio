c5673dbc5ce8ea22cbcb2fa1bd3fbd25425563ed
BarzaGuignol <yayav.yaya@gmail.com>
2023-02-03 11:09:39 +0100

    ajout composant footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div>
-      <h2>Footer</h2>
-    </div>
+    <footer className="min-h-20 bg-[#1C2833] p-6 place-content-center">
+      <div>
+        <p className="text-center text-[#3498DB]">Copyright Yanis Viot © 2023 - All rights reserved</p>
+      </div>
+      <div className="w-[80px] mx-auto mt-4">
+        <a className="text-[#FFC300]" href="" target="_blank">Le balcon</a>
+      </div>
+    </footer>
   );
 }
