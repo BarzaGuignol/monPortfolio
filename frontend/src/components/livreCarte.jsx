974558487e814386804d56a4750f02ef6e9dcebe
BarzaGuignol <yayav.yaya@gmail.com>
2023-02-04 13:44:33 +0100

    mise en place des écrits sur page passion

---
+++ frontend/src/components/livreCarte.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/prop-types */
+
+import React from "react";
+
+export default function ProjetCarte({ data }) {
+  return (
+    <div className="flex flex-col w-[200px] h-[400px] mb-20 space-x-12">
+      <div className="h-20 mb-8">
+        <h2 className="text-2xl text-[#FFC300] font-bold">{data.titreLivre}</h2>
+      </div>
+      <div className="">
+        <img
+          src={data.imageLivre}
+          alt={data.titreLivre}
+          className="text-[#1C2833] border-4 border-[#1C2833] rounded-lg"
+        />
+      </div>
+      
+
+      {data.url_boutique ? (
+        <div className="flex flex-row justify-center">
+          <a
+            href={data.url_boutique}
+            target="_blank"
+            className="bg-transparent text-[#FFC300] text-center font-bold border-2 border-[#FFC300] mt-12 p-2 rounded hover:bg-[#FFC300] hover:opacity-80 hover:text-[#1C2833]"
+            rel="noreferrer"
+          >
+            Lien Boutique
+          </a>
+        </div>
+      ) : null}
+    </div>
+  );
+}
--- frontend/src/pages/Passion.jsx
+++ frontend/src/pages/Passion.jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import LivreCarte from "@components/LivreCarte";
+import apiConnexion from "../services/apiConnexion";
 
 export default function Passion() {
+  const [mesLivres, setMesLivres] = useState();
+  const [mesTableaux, setMesTableaux] = useState();
+
+  useEffect(() => {
+    apiConnexion
+      .get(`livres`)
+      .then((res) => {
+        setMesLivres(res.data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  useEffect(() => {
+    apiConnexion
+      .get(`tableaux`)
+      .then((res) => {
+        setMesTableaux(res.data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
-    <div>
-      <h2>Passion</h2>
+    <div className="min-h-screen bg-[#21618C]">
+      <h2 className="text-center text-[#3498DB] text-4xl pt-10 underline underline-offset-8 decoration-[#FFC300]">Mes écrits</h2>
+      <div className="flex flex-col sm:flex sm:flex-row sm:space-x-20 w-4/5 mx-auto text-center justify-center mt-20 pb-20">
+        {mesLivres?.map((data) => (
+            <LivreCarte key={data.id} data={data} />
+          ))}
+      </div>
+      <h2 className="text-center text-[#3498DB] text-4xl pt-10 underline underline-offset-8 decoration-[#FFC300]">Sélection des plus grands tableaux de l'histoire</h2>
+      <div>
+        
+      </div>
     </div>
   );
 }
